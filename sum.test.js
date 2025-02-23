a57803958361ce0efbe9800434922d4969fc7a8a
Slawomir Dyk <slawomir.dyk@astrosat.net>
2025-02-23 17:02:35 +0000

    test

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
 const add = require("./index");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
+test("adds 1 + 2 + 3 to equal 6", () => {
+  expect(add(1, 2, 3)).toBe(6);
 });
